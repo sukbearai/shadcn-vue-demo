d656be95e0b2e919f59e35186b2aab2cd112c133
suk.bear <suk.bear.suwu@gmail.com>
2024-06-09 08:43:49 +0800

    feat: add auth form

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+html, body , #__nuxt{
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <Button>Click me</Button>
+  </div>
+</template>
