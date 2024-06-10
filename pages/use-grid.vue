2a73ded2b439e3c24b3107e3e6383907e677aa90
suk.bear <suk.bear.suwu@gmail.com>
2024-06-10 10:41:56 +0800

    feat: add use grid

---
+++ pages/use-grid.vue
@@ -0,0 +1,6 @@
+<template>
+    <div
+        class="grid grid-cols-4 gap-4 text-white">
+        <div v-for="i in 9" class="p-4 rounded-lg shadow-lg bg-fuchsia-500">0{{ i }}</div>
+    </div>
+</template>
