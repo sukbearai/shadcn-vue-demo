5db8dd102c7094a383305fb7fe4dcd47e0d4560e
suk.bear <suk.bear.suwu@gmail.com>
2024-06-10 12:27:35 +0800

    update: grid

--- pages/use-grid.vue
+++ pages/use-grid.vue
@@ -1,6 +1,11 @@
 <template>
-    <div
-        class="grid grid-cols-4 gap-4 text-white">
-        <div v-for="i in 9" class="p-4 rounded-lg shadow-lg bg-fuchsia-500">0{{ i }}</div>
+    <div class="grid grid-cols-3 ... grid-flow-row-dense justify-items-center">
+      <div class="bg-fuchsia-500 p-4">01</div>
+      <div class="bg-fuchsia-500 p-4 justify-self-start">02</div>
+      <div class="grid grid-rows-subgrid gap-4 row-span-3">
+        <div class="bg-fuchsia-500 p-4 col-span-2 row-start-2">03</div>
+      </div>
+      <div class="bg-fuchsia-500 p-4">04</div>
+      <div class="bg-fuchsia-500 p-4 col-start-1 col-end-3">05</div>
     </div>
-</template>
+  </template>
